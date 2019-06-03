9ede11f624e897f5fdd352dd58a2aca3f7eea267
Luis Roel <luisroel@me.com>
2019-06-03 19:03:23 -0400

    Started work on custom user model

---
+++ Users/forms.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+# Import our user model
+from .models import ApplicationUser
+
+
+# User creation
+class ApplicationUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = ApplicationUser
+        fields = [
+            'email',
+            'sales_tax_rate',
+        ]
+
+
+# User editing
+class ApplicationUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = ApplicationUser
+        fields = [
+            'sales_tax_rate',
+        ]
