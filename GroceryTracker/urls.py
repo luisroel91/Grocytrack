00daa99abb954698a398818d00cc7dc57d0f0854
Luis Roel <luisroel@me.com>
2019-06-04 17:48:24 -0400

    Got JWT working, removed Swagger api app

--- GroceryTracker/urls.py
+++ GroceryTracker/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# Generate schema from our models + views
 from rest_framework.schemas import get_schema_view
 
 """
