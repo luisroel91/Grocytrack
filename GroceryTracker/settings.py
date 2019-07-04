ee666f2a0b1143f4ece3dc22a81dcffc63f18aa2
Luis Roel <luisroel@me.com>
2019-07-04 12:35:34 -0400

    Added Heroku

--- GroceryTracker/settings.py
+++ GroceryTracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fa^#q8qj^h&25l2)dwf^8)^7hhv&z%40bls-senxif_iqzssul')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ["*"]
 
