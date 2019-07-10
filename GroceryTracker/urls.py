6c4369579405457feda7c039e47f6c8e0470e26d
Luis Roel <luisroel@me.com>
2019-07-10 11:18:04 -0400

    Decoupled frontend again

--- GroceryTracker/urls.py
+++ GroceryTracker/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 # Import renderer to render schema as Swagger
 from rest_framework_swagger.views import get_swagger_view
