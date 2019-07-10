3443cdb48efe61bab0961b6fcf2b2b15d8c4fde7
Luis Roel <luisroel@me.com>
2019-07-10 14:56:42 -0400

    Made email templates for activation,etc

--- GroceryTracker/settings.py
+++ GroceryTracker/settings.py
@@ -189,8 +189,8 @@ PASSWORD_RESET_CONFIRM_URL = '/password/reset/confirm'
 
 # Djoser settings
 DJOSER = {
-    'PASSWORD_RESET_CONFIRM_URL': 'https://www.grocytrack.com/password-reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': 'https://www.grocytrack.com/activate/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': '/password-reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '/activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
     'SEND_CONFIRMATION_EMAIL': True,
     'EMAIL': {
