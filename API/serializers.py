e3a59264dc7fead7bd191bd7846643492a4c0b93
phaseon <luisroel@me.com>
2019-07-19 09:26:46 -0400

    Figuring out nestable writeable serializers

--- API/serializers.py
+++ API/serializers.py
@@ -10,10 +10,11 @@ from Lists.models import GroceryItemList
 
 
 class GroceryItemSerializer(serializers.ModelSerializer):
+    created_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
         model = GroceryItem
-        fields = ('name', 'price', 'upc')
+        fields = ('name', 'price', 'upc', 'created_by')
 
     lookup_field = 'upc'
 
@@ -21,20 +22,51 @@ class GroceryItemSerializer(serializers.ModelSerializer):
 # Serializer for GroceryItemLists
 
 class GroceryItemListSerializer (serializers.ModelSerializer):
+    items = GroceryItemSerializer(many=True)
+    created_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = GroceryItemList
-        fields = '__all__'
+        fields = ('items', 'sub_total', 'tax_amount', 'total_items', 'created_by')
 
     lookup_field = 'id'
 
+"""
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
 
+        instance = GroceryItemList.objects.create(**validated_data)
+
+        for item in items_data:
+            item_instance = GroceryItem.objects.create(item)
+            item_instance.save()
+            instance.items.add(item_instance)
+
+        instance.save()
+
+        return instance
+
+    def update(self, instance, validated_data):
+        items_data = validated_data.pop('items')
+
+        instance.items = validated_data.get('items')
+
+        for item in items_data:
+            if item in instance.items
+
+"""
 # Serializer for User objects
 
 class ApplicationUserSerializer(serializers.ModelSerializer):
+    lookup_field = "id"
+
+    user_lists = serializers.SerializerMethodField()
+
     class Meta:
         model = ApplicationUser
         fields = ('username', 'email', 'sales_tax_rate', 'user_lists')
 
-    lookup_field = "id"
+    def get_user_lists(self, obj):
+        lists = GroceryItemList.objects.filter(created_by_id=serializers.CurrentUserDefault())
 
-    user_lists = GroceryItemListSerializer(many=True, read_only=True)
+        return lists
